7055c0fd03ef241f389217e4b569220f7227a367
Eric Egan <egan1551@gmail.com>
2017-04-21 14:34:31 +0200

    --amend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './layouts/app.jsx';
-console.log('testing');
 ReactDOM.render(<App />, document.getElementById('app'));
