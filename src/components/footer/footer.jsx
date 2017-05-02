6a7b2673510c0979159c4649a30256d946ea3ac6
Annika Svedin <annika.svedin@gmail.com>
2017-05-02 14:01:45 +0200

    moved the links into the footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './footer.scss';
+import CSSModules from 'react-css-modules';
 
 class Footer extends React.Component {
   render() {
     return(
-      <footer>
-        <h1>Footer</h1>
-      </footer>
+      <div styleName="menuposition">
+        <ul>
+          <NavLink styleName="navigation-link" to="/categories">Categories</NavLink>
+          <NavLink styleName="navigation-link" to="/settings">Settings</NavLink>
+          <NavLink styleName="navigation-link" to="/events">Events</NavLink>
+          <NavLink styleName="navigation-link" to="/favourites">Favourits</NavLink>
+        </ul>
+      </div>
     );
   }
 }
 
-export default Footer;
+export default CSSModules(Footer, styles);
--- src/layouts/default_layout/default-layout.jsx
+++ src/layouts/default_layout/default-layout.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Header from '../../containers/header_container/header-container.js';
+import Footer from '../../containers/footer_container/footer-container.js';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
-import styles from './default-layout.scss';
-import CSSModules from 'react-css-modules';
 
 class DefaultLayout extends React.Component {
   render() {
@@ -13,14 +11,7 @@ class DefaultLayout extends React.Component {
 
         { this.props.children }
 
-        <div styleName="menuposition">
-          <ul>
-            <NavLink styleName="navigation-link" to="/categories">Categories</NavLink>
-            <NavLink styleName="navigation-link" to="/settings">Settings</NavLink>
-            <NavLink styleName="navigation-link" to="/events">Events</NavLink>
-            <NavLink styleName="navigation-link" to="/favourites">Favourits</NavLink>
-          </ul>
-        </div>
+        <Footer />
       </div>
     );
   }
@@ -30,4 +21,4 @@ DefaultLayout.propTypes = {
   children: PropTypes.object,
 };
 
-export default CSSModules(DefaultLayout, styles);
+export default DefaultLayout;
