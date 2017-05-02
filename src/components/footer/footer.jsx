447c8031fb529877739d582863150dcd493b2b84
Annika Svedin <annika.svedin@gmail.com>
2017-05-02 14:45:38 +0200

    removed the ul tags

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,12 +7,10 @@ class Footer extends React.Component {
   render() {
     return(
       <div styleName="menuposition">
-        <ul>
-          <NavLink styleName="navigation-link" to="/categories">Categories</NavLink>
-          <NavLink styleName="navigation-link" to="/settings">Settings</NavLink>
-          <NavLink styleName="navigation-link" to="/events">Events</NavLink>
-          <NavLink styleName="navigation-link" to="/favourites">Favourits</NavLink>
-        </ul>
+        <NavLink styleName="navigation-link" to="/categories">Categories</NavLink>
+        <NavLink styleName="navigation-link" to="/settings">Settings</NavLink>
+        <NavLink styleName="navigation-link" to="/events">Events</NavLink>
+        <NavLink styleName="navigation-link" to="/favourites">Favourits</NavLink>
       </div>
     );
   }
