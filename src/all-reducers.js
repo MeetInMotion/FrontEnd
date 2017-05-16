0a9eec8520032ba46b5f1d369ed398d27fff3a83
Annika Svedin <annika.svedin@gmail.com>
2017-05-16 12:40:16 +0200

    finally our branch is fully merged with develop. Two lines were behind.

--- src/all-reducers.js
+++ src/all-reducers.js
@@ -5,6 +5,7 @@ import categories from './components/app/categories/category-reducer.js';
 import events from './components/app/events/event-reducer.js';
 import loginConnection from './components/app/login/login-reducer.js';
 import userInformation from './components/app/login/user-information';
+import locations from './components/app/locations/location-reducer.js';
 
 const allReducers = combineReducers({
   loginConnection,
@@ -13,6 +14,7 @@ const allReducers = combineReducers({
   footer,
   categories,
   events,
+  locations,
 });
 
 export default allReducers;
