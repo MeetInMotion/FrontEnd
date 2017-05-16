d057cb33a34570cc7021d38f17c0e907f51b2538
Cecilia To <ceto4157@student.su.se>
2017-05-16 15:31:05 +0200

    Changes to be committed: 	modified:   ../../../all-reducers.js

--- src/all-reducers.js
+++ src/all-reducers.js
@@ -5,7 +5,7 @@ import categories from './components/app/categories/category-reducer.js';
 import events from './components/app/events/event-reducer.js';
 import loginConnection from './components/app/login/login-reducer.js';
 import userInformation from './components/app/login/user-information';
-
+import locations from './components/app/locations/location-reducer.js';
 const allReducers = combineReducers({
   loginConnection,
   userInformation,
@@ -13,6 +13,7 @@ const allReducers = combineReducers({
   footer,
   categories,
   events,
+  locations,
 });
 
 export default allReducers;
