5d00e2b529e698434faa1196c96c020f70ccefd1
Eric Egan <egan1551@gmail.com>
2017-05-23 16:24:25 +0200

    added fetch-api

--- src/components/app/home/home.jsx
+++ src/components/app/home/home.jsx
@@ -5,6 +5,13 @@ class Home extends React.Component {
   componentWillMount() {
     const { loadingPage } = this.props;
     loadingPage();
+    fetch("http://api.localhost:8081/categories")
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(json){
+        console.log(json);
+      });
   }
 
   render() {
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
