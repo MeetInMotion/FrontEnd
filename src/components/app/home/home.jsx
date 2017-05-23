f04dee87fc49c5f78ff0425eb1a842ee930e4edc
Eric Egan <egan1551@gmail.com>
2017-05-23 16:25:17 +0200

    --amend

--- src/components/app/home/home.jsx
+++ src/components/app/home/home.jsx
@@ -5,13 +5,6 @@ class Home extends React.Component {
   componentWillMount() {
     const { loadingPage } = this.props;
     loadingPage();
-    fetch("http://api.localhost:8081/categories")
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(json){
-        console.log(json);
-      });
   }
 
   render() {
