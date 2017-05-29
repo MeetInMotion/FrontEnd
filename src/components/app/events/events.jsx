a64d8316c602eb32fa2b60a01405974576fb2c2e
Pontus <Pontussandliden@hotmail.com>
2017-05-30 00:25:49 +0200

    upcoming events now display date OR "Idag" or "Imorgon" and time

--- src/components/app/events/events.jsx
+++ src/components/app/events/events.jsx
@@ -3,6 +3,7 @@ import { PropTypes } from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import CSSModules from 'react-css-modules';
 import styles from './events.scss';
+import moment from 'moment';
 
 class Events extends React.Component{
 
@@ -11,6 +12,7 @@ class Events extends React.Component{
     actions.loadingPage('Events');
 
     if (this.props.locationId) {
+      console.log(this.props.locationId); //eslint-disable-line
       actions.loadLocationEvents(this.props.locationId);
     } else { 
       actions.loadUserEvents(1);
@@ -23,17 +25,25 @@ class Events extends React.Component{
       var eventB = b.datetime;
       return (eventA < eventB) ? -1 : (eventA > eventB) ? 1 : 0;
     });
+
+    
     return (
       <div>
-        <h2>
-          Kommande event
-        </h2>
+        <center>
+          <h2>
+            Kommande event
+          </h2>
+        </center>
         <ul className='list-events'>
           { eventList.map(
               (event, i) => (
                 <li className='list-group-item' key={ i }>
                   <NavLink to={ `/events/${event.id}` } >
-                    { event.title + " " + event.datetime}
+                    { event.title + ", " + moment().calendar(event.datetime, {
+                      sameDay: '[Idag]',
+                      lastDay: '[Imorgon]',
+                      sameElse: moment(event.datetime).format('YYYY-MM-DD'),
+                    }) + " " + moment(event.datetime).format("HH:mm")}
                   </NavLink>
                 </li>
               )
