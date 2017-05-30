721fa9d9d305234a4beda7be4bf02a6202ce1f89
Annika Svedin <annika.svedin@gmail.com>
2017-05-31 00:29:00 +0200

    almost done

--- src/components/footer/footer-container.js
+++ src/components/footer/footer-container.js
@@ -1,10 +1,28 @@
 import { connect } from 'react-redux';
 import Footer from './footer.jsx';
+import {
+  loadFbSdk,
+} from './footer-actions';
+
+import {
+  signUserOut,
+  authenticateUser,
+} from '../header/user-actions';
+
 
 function mapStateToProps(state) {
   return {
     footer: state.footer,
+    user: state.user,
   };
 }
 
-export default connect(mapStateToProps)(Footer);
+function mapDispatchToProps(dispatch){
+  return {
+    loadFbSdk: () => dispatch(loadFbSdk()), 
+    authenticateUser: (token) => dispatch(authenticateUser(token)),
+    signUserOut: () => dispatch(signUserOut()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
