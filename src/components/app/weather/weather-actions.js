e0a6c75b9ed25ade66143ee9053dc3eaee466386
Annika Svedin <annika.svedin@gmail.com>
2017-05-31 13:27:23 +0200

    eslint please

--- src/components/app/weather/weather-actions.js
+++ src/components/app/weather/weather-actions.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_KEY = '3301c5b3eb5119f400173a458088c41a';
-const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-
 export const FETCHING_WEATHER = 'FETCHING_WEATHER';
 export function fetchWeather(city) {
+  const API_KEY = '3301c5b3eb5119f400173a458088c41a';
+  const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+  
   return function(dispatch) {
     return dispatch({
       type: FETCHING_WEATHER,
@@ -16,6 +16,8 @@ export function fetchWeather(city) {
     });
   };
 }
+
+
 // export function fetchingWeather(promise) {
 //   return {
 //     type: FETCHING_WEATHER,
