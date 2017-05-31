99c492523e3c698271e981e91c7bfd234cd7d639
Annika Svedin <annika.svedin@gmail.com>
2017-05-31 02:58:59 +0200

    render footer-events only when logged in

--- src/components/app/events/events.jsx
+++ src/components/app/events/events.jsx
@@ -9,9 +9,7 @@ class Events extends React.Component {
 
   constructor(props) {
     super(props);
-
-    // this.authentication = this.authentification(this);
-    // this.renderEvents = this.renderEvents(this);
+    this.authentication = this.authentification(this);
   }
 
   componentWillMount() {
@@ -25,47 +23,24 @@ class Events extends React.Component {
     }
   }
 
-  // authentification() {
-  //   console.log(this.props.user.authenticated);
-  //   if (this.props.user.authenticated == true) {
-  //     this.renderEvents();
-  //   } else {
-  //     return(
-  //       <div>Log in!
-  //       </div>
-  //     );
-  //   }
-  // }
+  authentification() {
+    console.log(this.props.user.authenticated);
+    if (this.props.user.authenticated == true) {
+      return(
+        <div>
+          { this.renderEvents() }
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          Log in!
+        </div>
+      );
+    }
+  }
 
-  // renderEvents() {
-  //   const { eventList } = this.props.events;
-  //   eventList.sort(function(a, b) {
-  //     var eventA = a.datetime;
-  //     var eventB = b.datetime;
-  //     return (eventA < eventB) ? -1 : (eventA > eventB) ? 1 : 0;
-  //   });
-
-  //   return (
-  //     <div>
-  //       <h2>
-  //         Kommande event
-  //       </h2>
-  //       <ul className='list-events'>
-  //         { eventList.map(
-  //             (event, i) => (
-  //               <li className='list-group-item' key={ i }>
-  //                 <NavLink to={ `/events/${event.id}` } >
-  //                   { event.title + " " + event.datetime}
-  //                 </NavLink>
-  //               </li>
-  //             )
-  //         )}
-  //       </ul>
-  //     </div>
-  //   );
-  // }
-
-  render() {
+  renderEvents() {
     const { eventList } = this.props.events;
     eventList.sort(function(a, b) {
       var eventA = a.datetime;
@@ -98,13 +73,13 @@ class Events extends React.Component {
     );
   }
 
-  // render() {
-  //   return(
-  //     <div>
-  //       { this.authentification() }
-  //     </div>
-  //   );
-  // }
+  render() {
+    return(
+      <div>
+        { this.authentification() }
+      </div>
+    );
+  }
 
   componentWillUnmount() {
     const { actions } = this.props;
