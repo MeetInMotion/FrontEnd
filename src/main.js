7c7ed8960ab37e7302526a91073e53782b7a69df
Annika Svedin <annika.svedin@gmail.com>
2017-05-31 23:57:31 +0200

    bye bye warning

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import Root from './root.jsx';
 import allReducers from './all-reducers.js';
-
 const store = createStore(
   allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(
-        thunkMiddleware
-  )
+  compose(applyMiddleware(thunk))
 );
 
-ReactDOM.render(<Root store={store} />, document.getElementById('app'));
+ReactDOM.render(
+  <Root store={ store } />,
+  document.getElementById('app')
+);
